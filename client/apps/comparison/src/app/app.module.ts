9b061e9fef37d2c220d02f5eba7f4c4d41bb12d6
Elena Kolesnikova <koleslena@yandex.ru>
2019-08-09 20:51:54 +0600

    added comparison ui

--- client/apps/comparison/src/app/app.module.ts
+++ client/apps/comparison/src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ComparisonModule} from "@groupdocs.examples.angular/comparison";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule,
+    ComparisonModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
